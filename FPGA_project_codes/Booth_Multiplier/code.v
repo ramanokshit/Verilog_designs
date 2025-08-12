871c4059573baf72c40b4c79d3bd39cb51812300
rama_nokshit <130278835+ramanokshit@users.noreply.github.com>
2025-08-12 21:59:36 +0530

    Update code.v

--- FPGA_project_codes/Booth_Multiplier/code.v
+++ FPGA_project_codes/Booth_Multiplier/code.v
@@ -1,17 +1,16 @@
-module Booth_Multiplier #(parameter n=10)(input signed [n-1:0] M, Q, input rst, output reg signed [2*n-1:0] P);
+module Booth_Multiplier #(parameter n=10)(input signed [n-1:0] M, Q, output reg signed [2*n-1:0] P);
   reg signed [n-1:0]A,Q1;
   integer i;
-  localparam k=$clog2(n);
   reg q;
-  always@(posedge rst) begin
-    A<=0; q<=0;Q1<=Q;
+  always@(*) begin
+    A=0; q=0;Q1=Q;
   for(i=0; i<n; i=i+1) begin
     case({Q1[0],q})
-      2'b10:A<=A-M;
-      2'b01:A<=A+M;
-      default:A<=A;
+      2'b10:A=A-M;
+      2'b01:A=A+M;
+      default: ;
     endcase
-    {A,Q1,q}={A,Q1,q}>>1;
+    {A,Q1,q}={A[n-1],A,Q1};
   end
   P={A,Q1};
   end
